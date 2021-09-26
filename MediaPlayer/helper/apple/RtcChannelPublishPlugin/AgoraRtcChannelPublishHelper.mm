7eacb11e35ebb1c5f7329fa2cfe4da403441e976
zhanxiaochao <768994664@qq.com>
2021-09-26 17:14:18 +0800

    update audio arithmetic

--- MediaPlayer/helper/apple/RtcChannelPublishPlugin/AgoraRtcChannelPublishHelper.mm
+++ MediaPlayer/helper/apple/RtcChannelPublishPlugin/AgoraRtcChannelPublishHelper.mm
@@ -12,6 +12,78 @@
 #import "AudioCircleBuffer.h"
 #import <mutex>
 
+namespace {
+typedef std::numeric_limits<int16_t> limits_int16;
+
+// The conversion functions use the following naming convention:
+// S16:      int16_t [-32768, 32767]
+// Float:    float   [-1.0, 1.0]
+// FloatS16: float   [-32768.0, 32767.0]
+// Dbfs: float [-20.0*log(10, 32768), 0] = [-90.3, 0]
+// The ratio conversion functions use this naming convention:
+// Ratio: float (0, +inf)
+// Db: float (-inf, +inf)
+static inline int16_t FloatToS16(float v) {
+  if (v > 0)
+    return v >= 1 ? limits_int16::max()
+                  : static_cast<int16_t>(v * limits_int16::max() + 0.5f);
+  return v <= -1 ? limits_int16::min()
+                 : static_cast<int16_t>(-v * limits_int16::min() - 0.5f);
+}
+const float kFourOddHarmonicsZeroGainTable[] = {
+    0.057728029f, 0.057896007f, 0.058399724f, 0.059238520f, 0.060411299f, 0.061916529f,
+    0.063752244f, 0.065916046f, 0.068405114f, 0.071216201f, 0.074345646f, 0.077789374f,
+    0.081542909f, 0.085601374f, 0.089959506f, 0.094611658f, 0.099551813f, 0.104773591f,
+    0.110270261f, 0.116034750f, 0.122059658f, 0.128337265f, 0.134859550f, 0.141618196f,
+    0.148604613f, 0.155809946f, 0.163225092f, 0.170840713f, 0.178647255f, 0.186634963f,
+    0.194793895f, 0.203113942f, 0.211584843f, 0.220196207f, 0.228937523f, 0.237798187f,
+    0.246767512f, 0.255834755f, 0.264989129f, 0.274219827f, 0.283516038f, 0.292866968f,
+    0.302261860f, 0.311690010f, 0.321140794f, 0.330603677f, 0.340068243f, 0.349524208f,
+    0.358961441f, 0.368369984f, 0.377740070f, 0.387062142f, 0.396326872f, 0.405525180f,
+    0.414648249f, 0.423687547f, 0.432634839f, 0.441482209f, 0.450222071f, 0.458847188f,
+    0.467350688f, 0.475726074f, 0.483967240f, 0.492068487f, 0.500024529f, 0.507830510f,
+    0.515482012f, 0.522975062f, 0.530306146f, 0.537472213f, 0.544470682f, 0.551299447f,
+    0.557956882f, 0.564441843f, 0.570753669f, 0.576892186f, 0.582857699f, 0.588650995f,
+    0.594273337f, 0.599726455f, 0.605012545f, 0.610134253f, 0.615094669f, 0.619897310f,
+    0.624546108f, 0.629045392f, 0.633399870f, 0.637614607f, 0.641695003f, 0.645646765f,
+    0.649475883f, 0.653188598f, 0.656791368f, 0.660290835f, 0.663693789f, 0.667007121f,
+    0.670237789f, 0.673392764f, 0.676478986f, 0.679503310f, 0.682472451f, 0.685392930f,
+    0.688271007f, 0.691112619f, 0.693923315f, 0.696708179f, 0.699471760f, 0.702217993f,
+    0.704950112f, 0.707670573f, 0.710380954f, 0.713081870f, 0.715772871f, 0.718452340f,
+    0.721117388f, 0.723763743f, 0.726385637f, 0.728975684f, 0.731524758f, 0.734021866f,
+    0.736454009f, 0.738806050f, 0.741060570f, 0.743197717f, 0.745195054f, 0.747027403f,
+    0.748666679f, 0.750081719f, 0.751238112f};
+
+#define ANTICLIP_MAX_INPUT_VALUE 4.0f  // The maximum input value to SRS Anticlip
+#define ANTICLIP_TBL_SIZE (sizeof(kFourOddHarmonicsZeroGainTable) / sizeof(float) - 1)  // 128
+#define OPERATION_THRESHOLD 0.02f
+
+void UniversalGainProcess(int16_t* audioIO, int blocksize, float gain){
+  int idx1, idx2;
+  float v, tmp, scale, scaleIdx1, scaleIdx2;
+  // A small upward correction for the gain factor
+  float apply_gain = gain * 1.1f * 3.0517578125E-5f;
+
+  for (blocksize--; blocksize >= 0; blocksize--) {
+    v = audioIO[blocksize] * apply_gain;
+    tmp = v >= 0 ? v : -v;
+    tmp *= (ANTICLIP_TBL_SIZE / ANTICLIP_MAX_INPUT_VALUE);
+    idx1 = (int)tmp;
+    idx2 = idx1 + 1;
+    if (idx1 >= (int)ANTICLIP_TBL_SIZE) {
+      idx1 = idx2 = ANTICLIP_TBL_SIZE;
+    }
+
+    scaleIdx1 = kFourOddHarmonicsZeroGainTable[idx1];
+    scaleIdx2 = kFourOddHarmonicsZeroGainTable[idx2];
+    scale = scaleIdx1 + (scaleIdx2 - scaleIdx1) * (tmp - idx1);  // 1st order linear interpolation
+    audioIO[blocksize] = FloatToS16(v * (1 - scale)) ;
+  }
+}
+
+}
+
+
 class AgoraAudioFrameObserver:public agora::media::IAudioFrameObserver
 {
 private:
@@ -145,19 +217,16 @@ public:
         play_audio_buf_->Pop(play_audio_mix_, bytes);
         int16_t* p16 = (int16_t*) play_audio_mix_;
         memcpy(play_buf_tmp_, audioFrame.buffer, bytes);
-        for (int i = 0; i < bytes / 2; ++i) {
-            play_buf_tmp_[i] += (p16[i] * playOutSignalValue_);
-            //audio overflow
-            if (play_buf_tmp_[i] > 32767) {
-                play_send_buf_[i] = 32767;
-            }
-            else if (play_buf_tmp_[i] < -32768) {
-                play_send_buf_[i] = -32768;
-            }
-            else {
-                play_send_buf_[i] = play_buf_tmp_[i];
-            }
+
+        for(int i = 0; i < bytes/2 ; i++){
+          int tmp1 = (play_buf_tmp_[i] / 2);
+          int tmp2 = (p16[i] * playOutSignalValue_ / 2);
+          play_buf_tmp_[i] = tmp1 + tmp2;
         }
+
+        // doLimiter
+        ::UniversalGainProcess(play_buf_tmp_,bytes/2, 2.0f);
+        
         memcpy(audioFrame.buffer, play_buf_tmp_,bytes);
     
         return true;
@@ -217,7 +286,7 @@ static AgoraRtcChannelPublishHelper *instance = NULL;
         [rtcEngine setVideoSource:self];
     }
     [rtcEngine setParameters:@"{\"che.audio.keep.audiosession\":true}"];
-    [rtcEngine setAudioProfile:AgoraAudioProfileMusicStandardStereo scenario:AgoraAudioScenarioChatRoomEntertainment];
+    [rtcEngine setAudioProfile:AgoraAudioProfileDefault scenario:AgoraAudioScenarioGameStreaming];
     [rtcEngine setRecordingAudioFrameParametersWithSampleRate:48000 channel:2 mode:AgoraAudioRawFrameOperationModeReadWrite samplesPerCall:960];
     [rtcEngine setPlaybackAudioFrameParametersWithSampleRate:48000 channel:2 mode:AgoraAudioRawFrameOperationModeReadWrite samplesPerCall:960];
 
